2a3186822aa22d472c6eac879154ca1f02286ae8
Alexander Resnik <resnikal71202@th-nuernberg.de>
2020-09-16 01:33:15 +0200

    added ask image

--- Hauptteil/sw/intro/ask.m
+++ Hauptteil/sw/intro/ask.m
@@ -1,8 +1,15 @@
 t = 0:0.1:100;
 subplot(3,1,1);
 plot(t,rectpuls(mod(t,33)/40));
+%set(ax,'xticklabel',[]);
+xlabel('Signal (binary)','FontSize',30);
+set(gca,'XTick',[], 'YTick', []);
 subplot(3,1,2);
 plot(t,sin(t));
+%set(ax,'xticklabel',[]);
+xlabel('carrier wave (433MHz)','FontSize',30);
+set(gca,'XTick',[], 'YTick', []);
 subplot(3,1,3);
 plot(t,(rectpuls(mod(t,33)/40).*sin(t)));
-
+xlabel('resulting ASK','FontSize',30);
+set(gca,'XTick',[], 'YTick', []);
